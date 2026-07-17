89b794510a1c6926e17a3be837c89d7e71b33ba1
Anton Strogonoff <anton@strogonoff.name>
2026-07-17 17:54:41 +0800

    chore(ops): update .pnp.cjs (doh)

--- .pnp.cjs
+++ .pnp.cjs
@@ -31,7 +31,7 @@ const RAW_RUNTIME_STATE =
   "ignorePatternData": "(^(?:\\\\.yarn\\\\/sdks(?:\\\\/(?!\\\\.{1,2}(?:\\\\/|$))(?:(?:(?!(?:^|\\\\/)\\\\.{1,2}(?:\\\\/|$)).)*?)|$))$)",\
   "pnpZipBackend": "libzip",\
   "fallbackExclusionList": [\
-    ["@metanorma/prosemirror-editor", ["virtual:23edede033594f2fe8011073850c9d8cdfbaa4c53bf7ad0f2fe4a5565c21efda938bfe0db9c764fc000ed5faf9d39649e5927e1202a9f84db0940eebeb03a1f6#workspace:pkg/prosemirror-editor", "workspace:pkg/prosemirror-editor"]],\
+    ["@metanorma/prosemirror-editor", ["workspace:pkg/prosemirror-editor"]],\
     ["@metanorma/prosemirror-schema", ["workspace:pkg/prosemirror-schema"]],\
     ["editor-gui", ["workspace:pkg/editor-gui"]],\
     ["metanorma-editor", ["workspace:."]]\
@@ -147,7 +147,7 @@ const RAW_RUNTIME_STATE =
     ],\
     [\
       "@metanorma/prosemirror-editor",\
-      "virtual:23edede033594f2fe8011073850c9d8cdfbaa4c53bf7ad0f2fe4a5565c21efda938bfe0db9c764fc000ed5faf9d39649e5927e1202a9f84db0940eebeb03a1f6#workspace:pkg/prosemirror-editor"\
+      "workspace:pkg/prosemirror-editor"\
     ],\
     [\
       "@metanorma/prosemirror-schema",\
@@ -188,6 +188,10 @@ const RAW_RUNTIME_STATE =
     [\
       "orderedmap",\
       "npm:2.1.1"\
+    ],\
+    [\
+      "prosemirror-commands",\
+      "npm:1.7.1"\
     ],\
     [\
       "prosemirror-model",\
@@ -531,55 +535,9 @@ const RAW_RUNTIME_STATE =
           ["prosemirror-model", "npm:1.25.11"],\
           ["prosemirror-state", "npm:1.4.4"],\
           ["prosemirror-view", "npm:1.42.0"],\
-          ["react", null],\
-          ["react-dom", null],\
-          ["react-reconciler", "virtual:198bdf7ace3892bf39afae733499df37bb92cdd06f52131186864ca14d2e2646e1d4d369d20e5fc1d8695f59ea21aa96e6bd9ec691de53b3de0c61d7563e10dc#npm:0.32.0"]\
-        ],\
-        "packagePeers": [\
-          "@tiptap/core",\
-          "@tiptap/pm",\
-          "@tiptap/react",\
-          "@types/prosemirror-model",\
-          "@types/prosemirror-state",\
-          "@types/prosemirror-view",\
-          "@types/react-dom",\
-          "@types/react-reconciler",\
-          "@types/react",\
-          "@types/tiptap__core",\
-          "@types/tiptap__pm",\
-          "@types/tiptap__react",\
-          "prosemirror-model",\
-          "prosemirror-state",\
-          "prosemirror-view",\
-          "react-dom",\
-          "react-reconciler",\
-          "react"\
-        ],\
-        "linkType": "HARD"\
-      }],\
-      ["virtual:228bbf7101eda76bbf77844c79cd350fdef54a87df08fdcea388651e0ead49d4cb5c441327ed4b15de223dda05ac98ac618a13da23e91b65634289a2812bfc98#npm:3.2.7", {\
-        "packageLocation": "./.yarn/__virtual__/@handlewithcare-react-prosemirror-virtual-25f6d48298/0/cache/@handlewithcare-react-prosemirror-npm-3.2.7-dc03ea487a-02b060be76.zip/node_modules/@handlewithcare/react-prosemirror/",\
-        "packageDependencies": [\
-          ["@handlewithcare/react-prosemirror", "virtual:228bbf7101eda76bbf77844c79cd350fdef54a87df08fdcea388651e0ead49d4cb5c441327ed4b15de223dda05ac98ac618a13da23e91b65634289a2812bfc98#npm:3.2.7"],\
-          ["@tiptap/core", null],\
-          ["@tiptap/pm", null],\
-          ["@tiptap/react", null],\
-          ["@types/prosemirror-model", null],\
-          ["@types/prosemirror-state", null],\
-          ["@types/prosemirror-view", null],\
-          ["@types/react", "npm:19.2.14"],\
-          ["@types/react-dom", "virtual:88c29522551529c36b8477cbce431298e4347033d97c5250d6eebb0d6f7e111658ba993aaa13312e5080f55b995fb3a44f1fe1df53e634c586b9f6959408dc76#npm:19.2.3"],\
-          ["@types/react-reconciler", null],\
-          ["@types/tiptap__core", null],\
-          ["@types/tiptap__pm", null],\
-          ["@types/tiptap__react", null],\
-          ["classnames", "npm:2.5.1"],\
-          ["prosemirror-model", "npm:1.25.11"],\
-          ["prosemirror-state", "npm:1.4.4"],\
-          ["prosemirror-view", "npm:1.42.0"],\
           ["react", "npm:19.2.7"],\
           ["react-dom", "virtual:88c29522551529c36b8477cbce431298e4347033d97c5250d6eebb0d6f7e111658ba993aaa13312e5080f55b995fb3a44f1fe1df53e634c586b9f6959408dc76#npm:19.2.7"],\
-          ["react-reconciler", "virtual:228bbf7101eda76bbf77844c79cd350fdef54a87df08fdcea388651e0ead49d4cb5c441327ed4b15de223dda05ac98ac618a13da23e91b65634289a2812bfc98#npm:0.32.0"]\
+          ["react-reconciler", "virtual:198bdf7ace3892bf39afae733499df37bb92cdd06f52131186864ca14d2e2646e1d4d369d20e5fc1d8695f59ea21aa96e6bd9ec691de53b3de0c61d7563e10dc#npm:0.32.0"]\
         ],\
         "packagePeers": [\
           "@tiptap/core",\
@@ -605,28 +563,6 @@ const RAW_RUNTIME_STATE =
       }]\
     ]],\
     ["@metanorma/prosemirror-editor", [\
-      ["virtual:23edede033594f2fe8011073850c9d8cdfbaa4c53bf7ad0f2fe4a5565c21efda938bfe0db9c764fc000ed5faf9d39649e5927e1202a9f84db0940eebeb03a1f6#workspace:pkg/prosemirror-editor", {\
-        "packageLocation": "./.yarn/__virtual__/@metanorma-prosemirror-editor-virtual-228bbf7101/1/pkg/prosemirror-editor/",\
-        "packageDependencies": [\
-          ["@handlewithcare/react-prosemirror", "virtual:228bbf7101eda76bbf77844c79cd350fdef54a87df08fdcea388651e0ead49d4cb5c441327ed4b15de223dda05ac98ac618a13da23e91b65634289a2812bfc98#npm:3.2.7"],\
-          ["@metanorma/prosemirror-editor", "virtual:23edede033594f2fe8011073850c9d8cdfbaa4c53bf7ad0f2fe4a5565c21efda938bfe0db9c764fc000ed5faf9d39649e5927e1202a9f84db0940eebeb03a1f6#workspace:pkg/prosemirror-editor"],\
-          ["@metanorma/prosemirror-schema", "workspace:pkg/prosemirror-schema"],\
-          ["@types/react", "npm:19.2.14"],\
-          ["@types/react-dom", "virtual:88c29522551529c36b8477cbce431298e4347033d97c5250d6eebb0d6f7e111658ba993aaa13312e5080f55b995fb3a44f1fe1df53e634c586b9f6959408dc76#npm:19.2.3"],\
-          ["prosemirror-model", "npm:1.25.11"],\
-          ["prosemirror-state", "npm:1.4.4"],\
-          ["prosemirror-view", "npm:1.42.0"],\
-          ["react", "npm:19.2.7"],\
-          ["react-dom", "virtual:88c29522551529c36b8477cbce431298e4347033d97c5250d6eebb0d6f7e111658ba993aaa13312e5080f55b995fb3a44f1fe1df53e634c586b9f6959408dc76#npm:19.2.7"],\
-          ["react-reconciler", "virtual:228bbf7101eda76bbf77844c79cd350fdef54a87df08fdcea388651e0ead49d4cb5c441327ed4b15de223dda05ac98ac618a13da23e91b65634289a2812bfc98#npm:0.32.0"],\
-          ["typescript", "patch:typescript@npm%3A6.0.3#optional!builtin<compat/typescript>::version=6.0.3&hash=5786d5"]\
-        ],\
-        "packagePeers": [\
-          "react-dom",\
-          "react"\
-        ],\
-        "linkType": "SOFT"\
-      }],\
       ["workspace:pkg/prosemirror-editor", {\
         "packageLocation": "./pkg/prosemirror-editor/",\
         "packageDependencies": [\
@@ -635,9 +571,12 @@ const RAW_RUNTIME_STATE =
           ["@metanorma/prosemirror-schema", "workspace:pkg/prosemirror-schema"],\
           ["@types/react", "npm:19.2.14"],\
           ["@types/react-dom", "virtual:88c29522551529c36b8477cbce431298e4347033d97c5250d6eebb0d6f7e111658ba993aaa13312e5080f55b995fb3a44f1fe1df53e634c586b9f6959408dc76#npm:19.2.3"],\
+          ["prosemirror-commands", "npm:1.7.1"],\
           ["prosemirror-model", "npm:1.25.11"],\
           ["prosemirror-state", "npm:1.4.4"],\
           ["prosemirror-view", "npm:1.42.0"],\
+          ["react", "npm:19.2.7"],\
+          ["react-dom", "virtual:88c29522551529c36b8477cbce431298e4347033d97c5250d6eebb0d6f7e111658ba993aaa13312e5080f55b995fb3a44f1fe1df53e634c586b9f6959408dc76#npm:19.2.7"],\
           ["react-reconciler", "virtual:198bdf7ace3892bf39afae733499df37bb92cdd06f52131186864ca14d2e2646e1d4d369d20e5fc1d8695f59ea21aa96e6bd9ec691de53b3de0c61d7563e10dc#npm:0.32.0"],\
           ["typescript", "patch:typescript@npm%3A6.0.3#optional!builtin<compat/typescript>::version=6.0.3&hash=5786d5"]\
         ],\
@@ -727,7 +666,7 @@ const RAW_RUNTIME_STATE =
       ["workspace:pkg/editor-gui", {\
         "packageLocation": "./pkg/editor-gui/",\
         "packageDependencies": [\
-          ["@metanorma/prosemirror-editor", "virtual:23edede033594f2fe8011073850c9d8cdfbaa4c53bf7ad0f2fe4a5565c21efda938bfe0db9c764fc000ed5faf9d39649e5927e1202a9f84db0940eebeb03a1f6#workspace:pkg/prosemirror-editor"],\
+          ["@metanorma/prosemirror-editor", "workspace:pkg/prosemirror-editor"],\
           ["@types/react", "npm:19.2.14"],\
           ["@types/react-dom", "virtual:88c29522551529c36b8477cbce431298e4347033d97c5250d6eebb0d6f7e111658ba993aaa13312e5080f55b995fb3a44f1fe1df53e634c586b9f6959408dc76#npm:19.2.3"],\
           ["editor-gui", "workspace:pkg/editor-gui"],\
@@ -796,6 +735,18 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "./.yarn/cache/orderedmap-npm-2.1.1-64b8a0684f-8d7d266659.zip/node_modules/orderedmap/",\
         "packageDependencies": [\
           ["orderedmap", "npm:2.1.1"]\
+        ],\
+        "linkType": "HARD"\
+      }]\
+    ]],\
+    ["prosemirror-commands", [\
+      ["npm:1.7.1", {\
+        "packageLocation": "./.yarn/cache/prosemirror-commands-npm-1.7.1-5b2afb6640-4884ea7a66.zip/node_modules/prosemirror-commands/",\
+        "packageDependencies": [\
+          ["prosemirror-commands", "npm:1.7.1"],\
+          ["prosemirror-model", "npm:1.25.11"],\
+          ["prosemirror-state", "npm:1.4.4"],\
+          ["prosemirror-transform", "npm:1.12.0"]\
         ],\
         "linkType": "HARD"\
       }]\
@@ -898,22 +849,8 @@ const RAW_RUNTIME_STATE =
         "packageLocation": "./.yarn/__virtual__/react-reconciler-virtual-db81ea2429/0/cache/react-reconciler-npm-0.32.0-9d60959fd4-ace0562d2a.zip/node_modules/react-reconciler/",\
         "packageDependencies": [\
           ["@types/react", "npm:19.2.14"],\
-          ["react", null],\
-          ["react-reconciler", "virtual:198bdf7ace3892bf39afae733499df37bb92cdd06f52131186864ca14d2e2646e1d4d369d20e5fc1d8695f59ea21aa96e6bd9ec691de53b3de0c61d7563e10dc#npm:0.32.0"],\
-          ["scheduler", "npm:0.26.0"]\
-        ],\
-        "packagePeers": [\
-          "@types/react",\
-          "react"\
-        ],\
-        "linkType": "HARD"\
-      }],\
-      ["virtual:228bbf7101eda76bbf77844c79cd350fdef54a87df08fdcea388651e0ead49d4cb5c441327ed4b15de223dda05ac98ac618a13da23e91b65634289a2812bfc98#npm:0.32.0", {\
-        "packageLocation": "./.yarn/__virtual__/react-reconciler-virtual-6a48372293/0/cache/react-reconciler-npm-0.32.0-9d60959fd4-ace0562d2a.zip/node_modules/react-reconciler/",\
-        "packageDependencies": [\
-          ["@types/react", "npm:19.2.14"],\
           ["react", "npm:19.2.7"],\
-          ["react-reconciler", "virtual:228bbf7101eda76bbf77844c79cd350fdef54a87df08fdcea388651e0ead49d4cb5c441327ed4b15de223dda05ac98ac618a13da23e91b65634289a2812bfc98#npm:0.32.0"],\
+          ["react-reconciler", "virtual:198bdf7ace3892bf39afae733499df37bb92cdd06f52131186864ca14d2e2646e1d4d369d20e5fc1d8695f59ea21aa96e6bd9ec691de53b3de0c61d7563e10dc#npm:0.32.0"],\
           ["scheduler", "npm:0.26.0"]\
         ],\
         "packagePeers": [\
